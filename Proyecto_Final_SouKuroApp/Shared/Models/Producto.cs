e2a75e32c96bd441dbe26794a74428ea1ed7b3b2
BrynOvalles <brynovallesburgos@gmail.com>
2024-03-21 21:53:07 -0400

    Creacion del Modelo Producto

---
+++ Proyecto_Final_SouKuroApp/Shared/Models/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class Producto
+	{
+		[Key]
+		public int ProductoId { get; set; }
+		[Required]
+		public string Nombre { get; set; } = string.Empty;
+		[Required]
+		public string Descripcion { get; set; } = string.Empty;
+		[Required]
+		public string Categoria { get; set; } = string.Empty;
+		[Required]
+		public string Proveedor { get; set; } = string.Empty;
+		[Required]
+		public decimal Precio { get; set; } = 0;
+		[Required]
+		public int Cantidad { get; set; } = 0;
+		[Required]
+		public DateTime Fecha { get; set; } = DateTime.Now;
+		[Required]
+		public bool Estado { get; set; } = false;
+	}
+}
