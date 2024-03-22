98f630605d71e338a0316931a7edeecadf964269
BrynOvalles <brynovallesburgos@gmail.com>
2024-03-21 22:06:24 -0400

    Creando el Contexto Vacio

---
+++ Proyecto_Final_SouKuroApp/Proyecto_Final_SouKuroApp/Proyecto_Final_SouKuroApp/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto_Final_SouKuroApp.DAL
+{
+	public class Contexto : DbContext
+	{
+		public Contexto(DbContextOptions<Contexto> options) : base(options)
+		{
+		}
+
+	}
+}
