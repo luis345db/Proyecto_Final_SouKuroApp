7c1d2496d6cade8915b846f629f521099585a212
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-21 22:33:00 -0400

    Agregue una llave foranea a El detalle de pedidos

--- Proyecto_Final_SouKuroApp/Shared/Models/Pedidos_Detalle.cs
+++ Proyecto_Final_SouKuroApp/Shared/Models/Pedidos_Detalle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace Shared.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [ForeignKey("PedidoId")]
 
         [Required]
         public string Nombre_Producto { get; set;}
