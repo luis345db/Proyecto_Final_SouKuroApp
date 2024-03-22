073f0e4776010673d8b010e6c1a60f2974bde4d2
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-21 21:57:46 -0400

    Creeacion del modelo de Pedidoss

---
+++ Proyecto_Final_SouKuroApp/Shared/Models/Pedidos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Pedidos
+    {
+        public int PedidoId { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public DateTime Fecha_Llegada { get; set; } = DateTime.Now;
+
+        [ForeignKey("Pedidos")]
+        public ICollection<Pedidos_Detalle> Detalle { get; set; } = new List<Pedidos_Detalle>();
+    }
+}
