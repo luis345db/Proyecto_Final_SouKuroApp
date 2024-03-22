8d45e69a9143576af93ff9156b26c3ece0c0c17f
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-21 21:40:05 -0400

    Cree el modelo de detalle del proveedor

---
+++ Proyecto_Final_SouKuroApp/Shared/Models/Proveedores_Detalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Proveedores_Detalle
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TipoProducto { get; set; }
+
+        [Required]
+        public string NombreProveedor { get; set; }
+    }
+}
