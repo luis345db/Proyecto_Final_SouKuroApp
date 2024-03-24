f47682a830a5c0e93c10adfe090904ec48413a64
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-23 21:20:34 -0400

    Agregue llave a pedidos

--- Proyecto_Final_SouKuroApp/Shared/Models/Pedidos.cs
+++ Proyecto_Final_SouKuroApp/Shared/Models/Pedidos.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shared.Models
-{
+namespace Shared.Models;
+
     public class Pedidos
     {
+        [Key]
         public int PedidoId { get; set; }
 
+        [Required]
         public int Cantidad { get; set; }
 
+        [Required]
         public DateTime Fecha_Llegada { get; set; } = DateTime.Now;
 
         [ForeignKey("Pedidos")]
         public ICollection<Pedidos_Detalle> Detalle { get; set; } = new List<Pedidos_Detalle>();
     }
-}
+
