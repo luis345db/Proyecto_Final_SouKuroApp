5ca58b2c8f43937a98b9dad044bc9ff18803bff7
BrynOvalles <brynovallesburgos@gmail.com>
2024-03-23 22:28:57 -0400

    Actualizando El Contexto

--- Proyecto_Final_SouKuroApp/Proyecto_Final_SouKuroApp/Proyecto_Final_SouKuroApp/DAL/Contexto.cs
+++ Proyecto_Final_SouKuroApp/Proyecto_Final_SouKuroApp/Proyecto_Final_SouKuroApp/DAL/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace Proyecto_Final_SouKuroApp.DAL
 {
@@ -7,6 +8,17 @@ namespace Proyecto_Final_SouKuroApp.DAL
 		public Contexto(DbContextOptions<Contexto> options) : base(options)
 		{
 		}
+		public DbSet<Compra> Compra { get; set; }
+		public DbSet<Empleado> empleados { get; set; }
+		public DbSet<Informe> Informe { get; set; }
+		public DbSet<Inventario> Inventario { get; set; }
+		public DbSet<Pedidos> Pedidos { get; set; }
+		public DbSet<Producto> Producto { get; set; }
+		public DbSet<Proveedores> Proveedores { get; set; }
+		public DbSet<Tipo_Producto> Tipo_Producto { get; set; }
+		public DbSet<Compras_Detalles> Compras_Detalles { get; set; }
+		public DbSet<Pedidos_Detalle> Pedidos_Detalle { get; set; }
+
 
 	}
 }
