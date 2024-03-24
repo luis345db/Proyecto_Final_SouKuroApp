f182d0cc3f5505db0151525620c21a795364b757
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-03-23 22:21:49 -0400

    Modelo Inventario campos

--- Proyecto_Final_SouKuroApp/Shared/Models/Inventario.cs
+++ Proyecto_Final_SouKuroApp/Shared/Models/Inventario.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.Models
 {
-    internal class Inventario
+    public class Inventario
     {
+        [Key]
+        public int InventarioId { get; set; }
+
+        [ForeignKey("Producto")]
+        public int ProductoId { get; set; }
+
+        [Required] 
+        public int Cantidad_Productos { get; set; }
+
+
     }
 }
