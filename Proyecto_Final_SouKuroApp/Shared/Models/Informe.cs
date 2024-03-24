9124b9f872c2b54a3087ec297453911f65d86d2c
BrynOvalles <brynovallesburgos@gmail.com>
2024-03-23 22:08:42 -0400

    Creando Informe

---
+++ Proyecto_Final_SouKuroApp/Shared/Models/Informe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+
+public class Informe
+{
+    [Key]
+    public int InformeId { get; set; }
+    [Required]
+    public DateTime Fecha_Inicio { get; set; } = DateTime.Now;
+    [Required]
+    public DateTime Fecha_Final { get; set; } = DateTime.Now;
+    [Required]
+    public decimal Total_Compras { get; set; } = 0;
+    [Required]
+    public decimal Gastado { get; set; } = 0;
+
+}
